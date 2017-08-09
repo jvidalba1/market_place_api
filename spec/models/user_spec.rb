5593c77120ed57229943b63a1f3fe6cd27c4f005
Mateo Vidal <jvidalba@gmail.com>
2017-08-09 11:38:57 -0500

    Adds show actiond the users controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe User, type: :model do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  describe 'validations' do 
+    context 'when email is not present'
+      it { is_expected.to validate_presence_of :email }
+      # it { is_expected.to validate_uniqueness_of :email }
+      it { is_expected.to validate_confirmation_of :password }
+      it { is_expected.to allow_value('example@domain.com').for(:email) }
+
+      it 'should be no valid' do 
+        user.email = " "
+        expect(user).not_to be_valid
+      end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,15 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'shoulda/matchers'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -29,6 +38,8 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
