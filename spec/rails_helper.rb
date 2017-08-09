cb7732c08391114fadbc1af37ebbdd81eb14b8c6
Mateo Vidal <jvidalba@gmail.com>
2017-08-09 14:09:07 -0500

    Adds sessions controller create action

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,6 +48,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  # config.include Devise::TestHelpers, :type => :controller DEPRECATED
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
