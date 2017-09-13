b388b594f6d91d537b3e461e40e733abae4b4092
Mateo Vidal <jvidalba@gmail.com>
2017-09-12 23:43:44 -0500

    Refactor Shipment validations

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -13,7 +13,6 @@ class Api::V1::ShipmentsController < ApplicationController
   #
   def create
     # binding.remote_pry
-    quantity = shipment_params["cantidad"].to_i
 
     # client = Client.find(shipment_params["cliente"])
 
@@ -21,34 +20,16 @@ class Api::V1::ShipmentsController < ApplicationController
       kind_vehicle: shipment_params["vehiculo"], 
       client_id: shipment_params["cliente"], 
       kind_service: shipment_params["servicio"], 
-      bodywork: shipment_params["carroceria"]
+      bodywork: shipment_params["carroceria"],
+      quantity: shipment_params["cantidad"].to_i
     })
-
-    services_ids = []
-    errors = []
-    if shipment.valid?
-      quantity.times do |t|
-        response = shipment.save
-        
-        if response["response"]
-          response["response"]["errors"].each {|error| errors.push(error)}
-        else
-          services_ids << response["data"]["shipment"]["shipment_id"]
-        end
-      end
-    else
-      errors = shipment.errors.messages.map do |key, value|
-        value.map do |v|
-        "#{key.to_s} #{v}"
-        end
-      end.flatten
-    end
+    
+    result = shipment.validate
   
-    if errors.any?
-      render json: { errors: errors.flatten.uniq }, status: 500
+    if result[:valid]
+      render json: result[:shipment_ids], status: 200
     else
-      data = { "services_ids": services_ids }
-      render json: data, status: 200
+      render json: { errors: result[:errors].flatten.uniq }, status: 500
     end
   end
 
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,10 +1,39 @@
 class Shipment < Wrapper::Base
   include ActiveModel::Validations
 
-  attr_accessor :kind_vehicle, :client_id, :kind_service, :bodywork
+  attr_accessor :kind_vehicle, :client_id, :kind_service, :bodywork, :quantity
 
   validates :kind_vehicle, presence: true
   validates :client_id, presence: true
   validates :bodywork, presence: true
+  validates :quantity, presence: true, numericality: true
 
+  def validate
+    shipment_ids = []
+    errors = []
+
+    if self.valid?
+      quantity.times do |t|
+        response = self.save
+        
+        if response["response"]
+          response["response"]["errors"].each {|error| errors.push(error)}
+        else
+          shipment_ids << response["data"]["shipment"]["shipment_id"]
+        end
+      end
+    else
+      errors = self.errors.messages.map do |key, value|
+        value.map do |v|
+        "#{key.to_s} #{v}"
+        end
+      end.flatten
+    end
+
+    if errors.any? 
+      { valid: false, errors: errors }
+    else
+      { valid: true, shipment_ids: shipment_ids}
+    end
+  end
 end
